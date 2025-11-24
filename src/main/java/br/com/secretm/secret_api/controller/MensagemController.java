40fd47258b562b522194805991792544aaa164f3
Pedro Alex <113649289+PedroAlex65@users.noreply.github.com>
2025-11-24 20:20:59 -0300

    Update MensagemController.java

--- src/main/java/br/com/secretm/secret_api/controller/MensagemController.java
+++ src/main/java/br/com/secretm/secret_api/controller/MensagemController.java
@@ -6,13 +6,8 @@ import br.com.secretm.secret_api.service.MensagemService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-<<<<<<< HEAD
 @CrossOrigin(origins = "https://secretme-frontend-react.vercel.app")
-=======
-@CrossOrigin(origins = "https://secretme-frontend-react-e9to.vercel.app")
->>>>>>> 462553d88fef2fae1bfcaef132afdc4eb9f4189a
 @RestController
 @RequestMapping("/api/mensagens")
 @ResponseStatus(HttpStatus.CREATED)
